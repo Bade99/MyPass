d4c46a051d228ead8bc656638ecf66aee76b0bc8
Bade99 <31745377+Bade99@users.noreply.github.com>
2026-05-01 02:03:51 -0300

    resource and config cleanup

--- protect/resource.h
+++ protect/resource.h
@@ -2,35 +2,40 @@
 // Microsoft Visual C++ generated include file.
 // Used by protect.rc
 //
-#define IDC_MYICON                      2
-#define IDD_PROTECT_DIALOG              102
-#define IDS_APP_TITLE                   103
-#define IDM_ABOUT                       104
-#define IDR_MAINFRAME                   128
+#define BMP_CALENDAR                    8
+#define BMP_EDIT                        9
 #define LANG_CONTROL_LOGIN              129
 #define LANG_NC_RESTORE                 130
-#define MYPASS_ICO_LOGO                 130
+#define ICO_LOGO                        130
 #define LANG_NC_MINIMIZE                131
-#define UNCAP_BMP_CLOSE                 131
+#define BMP_CLOSE                       131
 #define LANG_NC_MAXIMIZE                132
-#define UNCAP_BMP_MAX                   132
+#define BMP_MAX                         132
 #define LANG_NC_CLOSE                   133
-#define UNCAP_BMP_MIN                   133
-#define UNCAP_BMP_RIGHTARROW            134
+#define BMP_MIN                         133
 #define LANG_MENU_FILE                  134
+#define BMP_RIGHTARROW                  134
 #define LANG_MENU_SAVE                  135
-#define UNCAP_BMP_EARTH                 135
+#define BMP_EARTH                       135
 #define LANG_MENU_LANGUAGE              136
-#define UNCAP_BMP_CIRCLE                136
+#define BMP_CIRCLE                      136
 #define LANG_MENU_EDIT                  137
+#define BMP_ADD                         137
 #define LANG_MENU_EDIT_UNDO             138
 #define LANG_MENU_EDIT_REDO             139
+#define BMP_CLIPBOARD                   139
 #define LANG_LOGIN_USERNAME             140
+#define BMP_PADLOCK                     140
 #define LANG_LOGIN_PASSWORD             141
+#define BMP_CANCEL                      141
 #define LANG_EDIT_USERNAME_INVALIDCHARS 142
+#define BMP_EYE_OPEN                    142
 #define LANG_LOGIN_USERNAME_CONTROLCHARS 143
+#define BMP_EYE_CLOSED                  143
 #define LANG_LOGIN_USERNAME_STARTENDSPACE 144
+#define BMP_SEARCH                      144
 #define LANG_LOGIN_USERNAME_ENDDOT      145
+#define IDB_BITMAP1                     145
 #define LANG_LOGIN_USERNAME_EMPTY       146
 #define LANG_LOGIN_PASSWORD_EMPTY       147
 #define LANG_ERROR_PASSWORD             148
@@ -42,14 +47,39 @@
 #define LANG_UNSAVEDCHANGES_TXT         154
 #define LANG_SIGNUP                     155
 #define LANG_CREATEACCOUNT              156
-#define IDC_STATIC                      -1
+#define LANG_ERROR_SAVEFILE             157
+#define LANG_ERROR_SAVEFILE_PASSWORDS   157
+#define LANG_PWD_ED_EDIT                158
+#define LANG_PWD_ED_ADD                 159
+#define LANG_PWD_ED_DELETE              160
+#define LANG_PWD_ED_TBL_SHOW            161
+#define LANG_PWD_ED_TBL_COPY            162
+#define LANG_PWD_ED_TBL_DELETE          163
+#define LANG_PWD_ED_TBL_LOCK            164
+#define LANG_PWD_ED_TBL_USER            165
+#define LANG_PWD_ED_TBL_PASSWORD        166
+#define LANG_PWD_ED_TITLE               167
+#define LANG_PWD_ED_TBL_FIELD           168
+#define LANG_PWD_ED_TBL_VALUE           169
+#define LANG_SEARCH                     170
+#define LANG_COPY_TO_CLIPBOARD          171
+#define LANG_COPY_TO_CLIPBOARD_ERROR    172
+#define LANG_EDITOR_ADD                 173
+#define LANG_EDITOR_SORT_DATE_MODIFIED_NEWEST 174
+#define LANG_EDITOR_SORT_DATE_MODIFIED_OLDEST 175
+#define LANG_EDITOR_SORT_ALPHABETIC_AZ  176
+#define LANG_EDITOR_SORT_ALPHABETIC_ZA  177
+#define LANG_EDITOR_SORT_DATE_CREATED_NEWEST 178
+#define LANG_EDITOR_SORT_DATE_CREATED_OLDEST 179
+#define LANG_EDITOR_SORT                180
+#define LANG_PWD_ED_DATES               181
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        137
+#define _APS_NEXT_RESOURCE_VALUE        146
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
